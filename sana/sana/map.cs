3ed57deffa4075f6a32e1917ebc21570b296e1fa
test <test@CSIT.JUW>
2021-11-30 11:10:17 +0500

    lab

---
+++ sana/sana/map.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sana
+{
+    public class map : iMap
+    {
+        public void showLocation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
